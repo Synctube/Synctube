54845e8e5415aaa361fb74a6e0e718b7600f9710
Majiir Paktu <majiir@nabaal.net>
2014-01-12 04:43:32 -0500

    Load video metadata from YouTube JSONC API

--- videodata.js
+++ videodata.js
@@ -2,6 +2,8 @@
  * Module dependencies.
  */
 
+var pjson = require('./package.json');
+var request = require('request');
 var Video = require('./video.js');
 
 /**
@@ -18,6 +20,24 @@ function VideoData (video, length) {
 	Video.call(this, video.id);
 	this.length = length;
 }
+
+/**
+ * Creates an object with metadata for the given video.
+ */
+
+VideoData.Load = function (video, callback) {
+	request({
+		uri: 'http://gdata.youtube.com/feeds/api/videos/' + video.id + '?v=2&alt=jsonc',
+		headers: {
+			'User-Agent': pjson.name + '/' + pjson.version,
+		},
+		json: true,
+	}, function (err, res, body) {
+		err = err || body.error;
+		if (err) { callback(err); return; }
+		callback(null, new VideoData(video, body.data.duration));
+	});
+};
 
 /**
  * Extend Video.

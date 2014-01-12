570d2c07a35cf6821dc32de768d951c13172b6e9
Majiir Paktu <majiir@nabaal.net>
2014-01-12 04:41:32 -0500

    Video ID validation

--- video.js
+++ video.js
@@ -5,9 +5,19 @@
 module.exports = exports = Video;
 
 /**
+ * YouTube ID validation.
+ * See: http://markmail.org/message/jb6nsveqs7hya5la#query:+page:1+mid:jb6nsveqs7hya5la+state:results
+ */
+
+var validId = /^[a-zA-Z0-9_-]{11}$/;
+
+/**
  * Identifies a single video.
  */
 
 function Video (id) {
+	if (!validId.test(id)) {
+		throw new Error('Invalid video ID');
+	}
 	this.id = id;
 }

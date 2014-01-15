6142c5d6ccf9c84e429f988dc936f175fee0659c
Majiir Paktu <majiir@nabaal.net>
2014-01-15 08:33:31 -0500

    Room playlist runner synchronization over websockets

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 
 var sockets = require('./sockets.js');
 
+require('./sync.js');
+
 app.use(express.static(__dirname + '/static'));
 
 var server = app.listen(3000);
---
+++ sync.js
@@ -0,0 +1,110 @@
+/**
+ * Module dependencies.
+ */
+
+var asyncevent = require('./asyncevent.js');
+var PlaylistRunner = require('./playlistrunner.js');
+var rooms = require('./rooms.js');
+var safesocket = require('safesocket');
+var sockets = require('./sockets.js');
+var Video = require('./video.js');
+var VideoData = require('./videodata.js');
+
+/**
+ * Augment rooms with PlaylistRunner.
+ */
+
+rooms.on('create', function (room) {
+	room.runner = new PlaylistRunner();
+});
+
+rooms.on('destroy', function (room) {
+	room.runner.state.video = null;
+});
+
+/**
+ * Message formatters.
+ */
+
+function sendPlaylist (targets, runner) {
+	targets.emit('playlist', runner.playlist.toArray());
+}
+
+function sendState (targets, runner) {
+	targets.emit('state', runner.state);
+}
+
+/**
+ * Socket events.
+ */
+
+sockets.on('listen', function (io) {
+
+	rooms.on('create', function (room) {
+		var runner = room.runner;
+
+		runner.playlist.on('changed', function () {
+			sendPlaylist(io.sockets.in(room.name), runner);
+		});
+
+		var changed = asyncevent(function () {
+			sendState(io.sockets.in(room.name), runner);
+		});
+
+		['play', 'pause', 'seek', 'change'].forEach(function (emitter) {
+			runner.state.on(emitter, changed);
+		});
+	});
+
+	io.sockets.on('connection', function (socket) {
+
+		var name = socket.handshake.query.room;
+
+		socket.join(name);
+
+		var room = rooms.get(name).add(socket);
+		var runner = room.runner;
+
+		socket.on('disconnect', function () {
+			room.remove(socket);
+		});
+
+		socket.on('add', safesocket(1, function (id, callback) {
+			if (!Video.validId.test(id)) { callback(null); return; }
+			var video = new Video(id);
+			VideoData.Load(video, function (err, data) {
+				if (err) { callback(null); return; }
+				callback(runner.playlist.push(data));
+			});
+		}));
+
+		socket.on('delete', safesocket(1, function (id, callback) {
+			var entry = runner.playlist.find(id);
+			if (entry) {
+				runner.playlist.remove(entry);
+			}
+			callback(entry);
+		}));
+
+		socket.on('move', safesocket(3, function (id, forward, skips, callback) {
+			if (!(skips instanceof Array)) { callback(null); return; }
+			var entry = runner.playlist.find(id);
+			if (entry) {
+				runner.playlist.skipmove(entry, forward, skips);
+			}
+			callback(entry);
+		}));
+
+		socket.on('cue', safesocket(1, function (id, callback) {
+			var entry = runner.playlist.find(id);
+			if (entry) {
+				runner.cueVideo(entry);
+			}
+			callback(entry);
+		}));
+
+		sendPlaylist(socket, runner);
+		sendState(socket, runner);
+	});
+
+});

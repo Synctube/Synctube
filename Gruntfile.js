95bd5f47365bbd5bc76a05cc53f6e2db5330bc16
Majiir Paktu <majiir@nabaal.net>
2014-01-18 12:34:46 -0500

    Use Grunt to watch for file changes and run tasks and servers accordingly

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,77 @@
 module.exports = function (grunt) {
 
+	var lessFiles = {
+		'static/style.css': 'less/main.less',
+	};
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		less: {
+			options: {
+				paths: [
+					'less',
+					'bower_components',
+				],
+			},
+			dev: {
+				files: lessFiles,
+			},
 			dist: {
 				options: {
-					paths: [
-						'less',
-						'bower_components',
-					],
 					compress: true,
 					cleancss: true,
 					report: 'min',
 				},
-				files: {
-					'static/style.css': 'less/main.less',
+				files: lessFiles,
+			},
+		},
+		watch: {
+			less: {
+				files: ['less/**/*.less'],
+				tasks: ['less:dev'],
+				options: {
+					atBegin: true,
+					interrupt: true,
+				},
+			},
+			static: {
+				files: [
+					'static/**/*',
+					'template/**/*',
+				],
+				options: {
+					livereload: true,
+				},
+			},
+		},
+		nodemon: {
+			dev: {
+				script: 'server.js',
+				options: {
+					ignored: [
+						'Gruntfile.js',
+						'node_modules/**',
+					],
+					watchedExtensions: ['js'],
+				},
+			},
+		},
+		concurrent: {
+			dev: {
+				tasks: ['watch', 'nodemon:dev'],
+				options: {
+					logConcurrentOutput: true
 				},
 			},
 		},
 	});
 
+	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-nodemon');
 
-	grunt.registerTask('default', ['less']);
+	grunt.registerTask('default', ['less:dist']);
+	grunt.registerTask('dev', ['concurrent:dev']);
 
 };

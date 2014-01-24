462f9660c1699ba4a06dfb0d5e5e1f60b3ee5163
Majiir Paktu <majiir@nabaal.net>
2014-01-23 19:15:37 -0500

    Simple rooms listing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var mu = require('mu2');
 
 var sockets = require('./server/sockets.js');
+var rooms = require('./lib/rooms.js');
 
 require('./server/sync.js');
 
@@ -14,6 +15,25 @@ function render(res, file, data) {
 	}
 	mu.compileAndRender(file, data).pipe(res);
 }
+
+app.get('/rooms', function (req, res) {
+	var data = {
+		rooms: rooms.toArray().map(function (room) {
+			return {
+				name: room.name,
+				connected: Object.keys(room.connected).length,
+			};
+		}),
+	};
+	res.format({
+		json: function (req, res) {
+			res.json(data);
+		},
+		html: function (req, res) {
+			render(res, 'rooms.html', data);
+		},
+	});
+});
 
 app.get('/rooms/:name', function (req, res) {
 	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' });

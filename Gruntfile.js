fda443d262013a4edf5d907d8d7ed71e89560fd7
Majiir Paktu <majiir@nabaal.net>
2015-04-26 20:06:52 -0400

    Replace Grunt build tasks with npm scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,96 +1,8 @@
 module.exports = function (grunt) {
 
-	var lessFiles = {
-		'static/style.css': 'less/main.less',
-	};
-
-	var roomFiles = ['client/room.js'];
-	var roomsFiles = ['client/rooms.js'];
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		browserify: {
-			dev: {
-				files: {
-					'static/room.js': roomFiles,
-					'static/rooms.js': roomsFiles,
-				},
-				options: {
-					debug: true,
-				},
-			},
-			dist: {
-				files: {
-					'build/room.js': roomFiles,
-					'build/rooms.js': roomsFiles,
-				},
-			},
-		},
-		uglify: {
-			dist: {
-				options: {
-					report: 'min',
-					preserveComments: 'some',
-					banner: '/*! Copyright (c) Henry Merriam 2014. All Rights Reserved. */\n',
-				},
-				files: {
-					'static/room.js': ['build/room.js'],
-					'static/rooms.js': ['build/rooms.js'],
-				},
-			},
-		},
-		less: {
-			options: {
-				paths: [
-					'less',
-					'bower_components',
-				],
-			},
-			dev: {
-				files: lessFiles,
-				options: {
-					sourceMap: true,
-					sourceMapFilename: 'static/style.css.map',
-					sourceMapURL: 'style.css.map',
-				},
-			},
-			dist: {
-				options: {
-					compress: true,
-					cleancss: true,
-					report: 'min',
-				},
-				files: lessFiles,
-			},
-		},
 		watch: {
-			browserify: {
-				files: [
-					'client/**/*.js',
-					'lib/*.js',
-				],
-				tasks: ['browserify:dev'],
-				options: {
-					atBegin: true,
-					interrupt: true,
-				},
-			},
-			less: {
-				files: ['less/**/*.less'],
-				tasks: ['less:dev'],
-				options: {
-					atBegin: true,
-					interrupt: true,
-				},
-			},
-			mustache: {
-				files: ['template/**/*'],
-				tasks: ['mustache_render:dist'],
-				options: {
-					atBegin: true,
-					interrupt: true,
-				},
-			},
 			static: {
 				files: [
 					'static/**/*',
@@ -125,14 +37,10 @@ module.exports = function (grunt) {
 		},
 	});
 
-	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-concurrent');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-nodemon');
 
-	grunt.registerTask('default', ['less:dist', 'browserify:dist', 'uglify:dist']);
-	grunt.registerTask('dev', ['concurrent:dev']);
+	grunt.registerTask('default', ['concurrent:dev']);
 
 };

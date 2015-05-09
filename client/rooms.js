7464997030a5039c34e4468dd536f73bdbefe61b
Majiir Paktu <majiir@nabaal.net>
2015-05-09 17:18:33 -0400

    Fix room name encoding on join

--- client/rooms.js
+++ client/rooms.js
@@ -12,7 +12,7 @@ var rooms = module.exports = exports = new (function () {
 	var self = this;
 	self.name = ko.observable('');
 	self.create = function () {
-		var name = encodeURI(self.name());
+		var name = encodeURIComponent(self.name());
 		if (name) {
 			window.location.pathname = '/rooms/' + name;
 		}

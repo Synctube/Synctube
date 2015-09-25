2b2e619e9a4948b054d5b5e54b766ecb0cba2ce4
Majiir Paktu <majiir@nabaal.net>
2015-09-25 03:08:49 -0400

    Extract controls view model

--- client/playlist.js
+++ client/playlist.js
@@ -95,22 +95,9 @@ entries.on('remove', updatePlaylist);
 var playlist = module.exports = exports = new (function () {
 	var self = this;
 	self.entries = ko.observableArray();
-	self.seek = function () {
-		sync.seek(moment.duration(self.seekTime()).asSeconds());
-	};
-	self.seekTime = ko.observable('');
-	self.playpause = function () {
-		if (self.playing()) {
-			sync.pause();
-		} else {
-			sync.play();
-		}
-	};
-	self.playing = ko.observable(false);
 	self.currentKey = ko.observable(null);
 	sync.state.on('state', function () {
 		var state = sync.state.getState();
-		self.playing(state.playing);
 		self.currentKey(state.key || null);
 	});
 	self.shuffle = function () {
--- client/room.js
+++ client/room.js
@@ -4,6 +4,7 @@
 
 var domready = require('domready');
 var ko = require('knockout');
+var controls = require('./controls');
 var playlist = require('./playlist');
 var search = require('./search');
 
@@ -12,6 +13,7 @@ var search = require('./search');
  */
 
 var room = module.exports = exports = {
+	controls: controls,
 	playlist: playlist,
 	search: search,
 };
